f00acbae715b3a14127cb0f4a35e64f0b95811dc
mostafa abdelrasool <mostafa.abdelrasoul.89@gmail.com>
2020-01-23 20:02:49 +0200

    code generator

--- src/app/code-generator/angular/HtmlCodeService.ts
+++ src/app/code-generator/angular/HtmlCodeService.ts
@@ -10,8 +10,34 @@ export class HtmlCodeService {
     inputCode(field: Fields): string {
         return `<mat-form-field [(ngModel)]="${field.model}" id="${field.id}" [style]="${this.getStyle(field.style)}" 
     class="${field.classes ? field.classes.join(' ') : ''}">
-     <input type="${this.getTypeName(field.type)}" matInput placeholder="${field.placeholder || ''}" value="${field.value || ''}">
+     <input [required]="${field.required||''}" type="${this.getTypeName(field.type)}" matInput placeholder="${field.placeholder || ''}" value="${field.value || ''}">
   </mat-form-field>`;
+    }
+    checkBoxCode(field: Fields): string {
+        return `<mat-checkbox  [required]="${field.required||''}" [(ngModel)]="${field.model}" id="${field.id}" [style]="${this.getStyle(field.style)}" 
+        class="${field.classes ? field.classes.join(' ') : ''}">${field.placeholder || ''}</mat-checkbox>`;
+    }
+    datepickerCode(field: Fields): string {
+        return ` <mat-form-field   id="${field.id}" [style]="${this.getStyle(field.style)}" 
+        class="${field.classes ? field.classes.join(' ') : ''}">
+        <input readonly="true" [required]="${field.required||''}" [min]="${field.min}" [max]="${field.max}" [(ngModel)]="${field.model}" matInput [matDatepicker]="picker" placeholder="${field.placeholder || ''}">
+        <mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
+        <mat-datepicker #picker startView="year"></mat-datepicker>
+    </mat-form-field>`;
+    }
+    RadioButtonGroupCode(field: Fields): string {
+        let code = `<mat-radio-group [required]="${field.required||''}" fxLayout="row" fxLayoutGap="5px" [(ngModel)]="${field.model}"  id="${field.id}" [style]="${this.getStyle(field.style)}" 
+        class="${field.classes ? field.classes.join(' ') : ''}">`;
+        field.radioButtonGroup.forEach(r => {
+            code += `<mat-radio-button value="${field.value}">${field.placeholder || ''}</mat-radio-button>`
+        })
+        code += '</mat-radio-group>'
+        return code;
+    }
+    sliderCode(field: Fields): string {
+        return `<div> <label>${field.placeholder}</label>
+        <mat-slider  id="${field.id}" [style]="${this.getStyle(field.style)}" 
+        class="${field.classes ? field.classes.join(' ') : ''}" [max]="${field.max}" [min]="${field.min}" [step]="${field.step}"></mat-slider></div>`;
     }
     divCode(container: Containers): string {
         let code = `<div class="${container.classes ? container.classes.join(' ') : ''}" [style]="${this.getStyle(container.style)}">`;
@@ -21,7 +47,18 @@ export class HtmlCodeService {
                 case FieldType.INPUT_TEXT:
                     code += "\n" + this.inputCode(x)
                     break;
-
+                case FieldType.CHECKBOX:
+                    code += '\n' + this.checkBoxCode(x);
+                    break;
+                case FieldType.DATEPICKER:
+                    code += '\n' + this.datepickerCode(x);
+                    break;
+                case FieldType.RADIO_BUTTON_GROUP:
+                    code += '\n' + this.RadioButtonGroupCode(x);
+                    break;
+                case FieldType.SLIDER:
+                    code += '\n' + this.sliderCode(x);
+                    break;
                 default:
                     break;
             }
